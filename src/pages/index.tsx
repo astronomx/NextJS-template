9f10784a32597929dd104ea2401778271587ee63
astronomx <unkownusernotfound@protonmail.com>
2023-04-21 16:07:14 +0200

    Added boilerplate text for tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,7 @@
-import Head from "next/head";
-
-import Navbar from "@/components/Navbar";
-import Herobanner from "@/components/Herobanner";
-
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Home - Stagelogboek</title>
-      </Head>
-      <div className="absolute z-10 w-full">
-        <Navbar />
-      </div>
-      <div className="relative z-0">
-        <Herobanner />
-      </div>
+      <h1 className="flex justify-center mt-10 font-bold">If this text is centered and bold tailwind is installed</h1>
     </>
   )
 }
